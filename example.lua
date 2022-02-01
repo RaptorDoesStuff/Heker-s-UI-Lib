0220514ded541c97717d2db9b6a157c453683375
RaptorDoesStuff <56210552+RaptorDoesStuff@users.noreply.github.com>
2022-01-31 23:17:13 -0700

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,6 +1,7 @@
 loadstring(syn.request({Url="https://raw.githubusercontent.com/RaptorDoesStuff/Heker-s-UI-Lib/main/api.lua",Method="GET"}).Body)()
 local gui = huiCreateGui({
-    Style = huiStyleDefault
+    Style = huiStyleDefault,
+    InitialTab = 1
 })
 gui:AddTab("Home")
 gui:AddTab("Scripts")
